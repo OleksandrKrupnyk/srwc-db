5c38d42f5a054b28eaf9c4ec5e0b5a353c2566ac
Alex Krupnik <krupnik_a@ukr.net>
2020-01-06 22:07:16 +0200

    Change method read settings

--- db/src/zukr/base/Logger.php
+++ db/src/zukr/base/Logger.php
@@ -32,11 +32,11 @@ class Logger implements LoggerInterface
     {
         $dateFormat = "H:i:s.u";
         // the default output format is "[%datetime%] %channel%.%level_name%: %message% %context% %extra%\n"
-        $output = "%datetime% > %level_name% > %message% %context%".PHP_EOL;
+        $output = "%datetime% > %level_name% > %message% %context%" . PHP_EOL;
         // finally, create a formatter
         $formatter = new LineFormatter($output, $dateFormat);
-
-        $stream = new StreamHandler('d:\elm.log', MonologLogger::DEBUG);
+        $logFile = getenv('LOG_FILE') ?? '/var/log/elm.log';
+        $stream = new StreamHandler($logFile, MonologLogger::DEBUG);
         $stream->setFormatter($formatter);
 
         $logger = new MonologLogger('elm');
