55713347e94d59aae7c909ab6755f7c616434b3f
Alex Krupnik <krupnik_a@ukr.net>
2020-01-09 02:28:44 +0200

    Main page design style

--- db/app/index.php
+++ db/app/index.php
@@ -1,17 +1,19 @@
 <?php
 
-use zukr\base\Base;
 
 require './../admin/config.inc.php';
 require './../admin/functions.php';
 require '../vendor/autoload.php';
 
+use zukr\base\Base;
+use zukr\base\helpers\ArrayHelper;
+
 Base::init();
-$settings = Base::$param;
+$settings = ArrayHelper::merge($settings, Base::$param->getAllsettingValue());
 global $link;
 
 
-$setData = function () use ($link,$settings){
+$setData = function () use ($link, $settings) {
     $query = "
 SELECT w.id,
        w.title,
@@ -70,59 +72,57 @@ FROM `works` AS w
 GROUP BY w.id
 ORDER BY w.id        
         ";
-        $data = [];
-        mysqli_query($link, "SET NAMES 'utf8'");
-        mysqli_query($link, "SET CHARACTER SET 'utf8'");
-        $result = mysqli_query($link, $query);
-        // echo "<pre>{$query}</pre>";
-        while ($row = mysqli_fetch_array($result)) {
-            $s = [];
-            $diplomas = [];
+    $data = [];
+    $result = mysqli_query($link, $query);
+    // echo "<pre>{$query}</pre>";
+    while ($row = mysqli_fetch_array($result)) {
+        $s = [];
+        $diplomas = [];
 
-            $s['id'] = $row['id'];
-            $s['date'] = $row['date'];
+        $s['id'] = $row['id'];
+        $s['date'] = $row['date'];
 
-            $s['title'] = (
-                '1' === $settings['SHOW_FILES_LINK'] // Настройка активна
-                &&
-                (
-                    ('D' !== $row['place1'] && $row['place1'] !== null)
-                    ||
-                    ('D' !== $row['place2'] && $row['place2'] !== null)
-                )
-                &&
-                $row['path'] !== null // Запись о файле есть в системе
-            ) ? "<a href='{$row['path']}' title='{$row['title']}'>{$row['title']}</a>" : $row['title'];
+        $s['title'] = (
+            '1' === $settings['SHOW_FILES_LINK'] // Настройка активна
+            &&
+            (
+                ('D' !== $row['place1'] && $row['place1'] !== null)
+                ||
+                ('D' !== $row['place2'] && $row['place2'] !== null)
+            )
+            &&
+            $row['path'] !== null // Запись о файле есть в системе
+        ) ? "<a href='{$row['path']}' title='{$row['title']}'>{$row['title']}</a>" : $row['title'];
 
 
-            $s['univer'] = $row['univer'];
-            $s['invitation'] = ($row['invitation']) ? "<span class='invite2018'>&nbsp; Авторів роботи запрошено до участі у конференції&nbsp;</span>" : '';
+        $s['univer'] = $row['univer'];
+        $s['invitation'] = ($row['invitation']) ? "<span class='invite2018'>&nbsp; Авторів роботи запрошено до участі у конференції&nbsp;</span>" : '';
 
-            // Дипломы
-            if ('D' !== $row['place1'] && $row['place1'] !== null) {
-                $diplomas [] = "<span class='invite2018'>&nbsp;Диплом&nbsp;{$row['place1']}-го&nbsp;ступеня</span>";
-            }
-
-            if ('D' !== $row['place2'] && $row['place2'] !== null) {
-                $diplomas [] = "<span class='invite2018'>&nbsp;Диплом&nbsp;{$row['place2']}-го&nbsp;ступеня</span>";
-            }
-
-            if (count($diplomas) > 0) {
-                $s['diploma'] = "<br>" . implode(", ", $diplomas);
-            }
-
-            $s['review'] = $row['reviewer1'] !== null ? "<a class='viewReview' href='index.php?action=review_view&id={$row['reviewer1']}'></a>" : '';
-            $s['review'] .= $row['reviewer2'] !== null ? "<a class='viewReview' href='index.php?action=review_view&id={$row['reviewer2']}'></a>" : '';
-            $s['review'] .= $row['balls'] !== null ? "/{$row['balls']}" : '';
-            $s['review'] .= $row['tesis'] == 1 ? "/<br>Отримано" : '';
-            $s['section'] = $row['section'];
-            $data [] = $s;
+        // Дипломы
+        if ('D' !== $row['place1'] && $row['place1'] !== null) {
+            $diplomas [] = "<span class='invite2018'>&nbsp;Диплом&nbsp;{$row['place1']}-го&nbsp;ступеня</span>";
         }
+
+        if ('D' !== $row['place2'] && $row['place2'] !== null) {
+            $diplomas [] = "<span class='invite2018'>&nbsp;Диплом&nbsp;{$row['place2']}-го&nbsp;ступеня</span>";
+        }
+
+        if (count($diplomas) > 0) {
+            $s['diploma'] = "<br>" . implode(", ", $diplomas);
+        }
+
+        $s['review'] = $row['reviewer1'] !== null ? "<a class='viewReview' href='index.php?action=review_view&id={$row['reviewer1']}'></a>" : '';
+        $s['review'] .= $row['reviewer2'] !== null ? "<a class='viewReview' href='index.php?action=review_view&id={$row['reviewer2']}'></a>" : '';
+        $s['review'] .= $row['balls'] !== null ? "/{$row['balls']}" : '';
+        $s['review'] .= $row['tesis'] == 1 ? "/<br>Отримано" : '';
+        $s['section'] = $row['section'];
+        $data [] = $s;
+    }
     return $data;
 };
 
 if (!isset($_GET['action'])) {
-    $data = Base::$app->cacheGetOrSet('table', $setData,30);
+    $data = Base::$app->cacheGetOrSet('table', $setData, 30);
 }
 ?>
 <!DOCTYPE html >
@@ -131,12 +131,11 @@ if (!isset($_GET['action'])) {
     <meta content="width=device-width, initial-scale=1" name="viewport">
     <meta content="Конкурс,СНР,Електротехніка,електромеханіка,ДДТУ,ЕЛМ,студентських,науових,робіт,конференція"
           name="keywords">
-    <meta
-        content="Електротехніка та електромеханіка - реєстр робіт Всеукраїнського конкурсу студентських наукових робіт"
-        name="description">
+    <meta content="Електротехніка та електромеханіка - реєстр робіт Всеукраїнського конкурсу студентських наукових робіт"
+          name="description">
     <?php include_once 'analyticstracking.php'; ?>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <link href="../css/userstyle.css" type="text/css" rel="stylesheet">
+    <link href="../css/userstyle.min.css" type="text/css" rel="stylesheet">
     <title><?= Base::$app->app_name ?></title>
 </head>
 <body>
@@ -156,19 +155,19 @@ if (!isset($_GET['action'])) {
     </h4>
     <h4><a href='http://elm-dstu-edu.org.ua/konkurs/index.php/digest/32-zbirnik-tez-2018'
            title='Збірник тез доповідей 2017-2018 н.р.'>Збірник тез доповідей 2018</a>&#8658;</h4>
-    <?php if('1' === $settings['SHOW_PROGRAMA']):?>
+    <?php if ('1' === $settings['SHOW_PROGRAMA']): ?>
         <h4><a href='./programa.php'>Макет програми конференції&#8658;</a></h4>
-    <?php endif;?>
+    <?php endif; ?>
 
-    <?php if('1' === $settings['INVITATION']):?>
+    <?php if ('1' === $settings['INVITATION']): ?>
         <h4><a href='./invitation.php'>Сторінка завантажень запрошень учасників конференції</a>&#8658;</h4>
-    <?php endif;?>
+    <?php endif; ?>
 
     <?php
     //echo "<h4><a href='http://elm-dstu-edu.org.ua/db/document3.pdf' title='МАКЕТ Збірника тез доповідей 2017-2018 н.р.'>Збірник тез доповідей 2018 [МАКЕТ від 13.04.18] &#8658;</a></h4>";
     //echo "<p>Просимо авторів тез первірити наявність публікації в МАКЕТІ збірника. У випадку, якшо вами були надіслані тези у форматі ТЕХ, але вони відсутні в МАКЕТІ збірника просимо повдомити про це на поштову скриньку <span class='invite2018'><a href=\"mailto:conkstudrabot@gmail.com\">conkstudrabot@gmail.com</span></a> до 14 квітня 2018р вказавши в тексті листа номер роботи та дату відправлення електронного листа з тезами.</p>";
     ?>
-    <?php if ('1' === $settings['SHOW_DB_TABLE']):?>
+    <?php if ('1' === $settings['SHOW_DB_TABLE']): ?>
         <h3>Роботи авторів та відомості про їх рецензування, а також наявність тез доповідей. Розподіл за секціями.</h3>
         <table style="border-collapse: collapse">
             <thead>
@@ -185,25 +184,31 @@ if (!isset($_GET['action'])) {
                     <td class='numero'><?= $d['id'] ?></td>
                     <td title="Останні зміни:<?= $d['date'] ?>" class='title'>
                         <?= $d['title'] ?>
-                        <br><?= $d['univer'] ?><?= $d['invitation'] ?><?= $d['diploma']??'' ?>
+                        <br><?= $d['univer'] ?><?= $d['invitation'] ?><?= $d['diploma'] ?? '' ?>
                     </td>
                     <td class='review'><?= $d['review'] ?></td>
                     <td class='section'><?= $d['section'] ?></td>
                 </tr>
             <?php endforeach; ?>
             </tbody>
+            <tfoot>
+            <tr>
+                <td colspan="4" style="border-color: white">* - Розподілення робіт по секціях може бути змінено після їх
+                    рецензування.
+                </td>
+            </tr>
+            </tfoot>
         </table>
-        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;* - Розподілення робіт по секціях може бути змінено після їх рецензування.</p>
-    <?php else:?>
-        <h1>Конкурс СНР 2018/2019 н.р. завершено. Результати переміщено в архів.</h1>
-<!--
-Закоментировав предидущую строку раскоментируй следующую (2018.06.13)
-<h1>Вибачте Реєст знаходиться в обробці. Завітайте на нашу сторінку пізніше.</h1>
-<h2>(Зараз. Роботи отримують рецензію. Після цього процесу та підбиття підсумків буде оприлюднено рейтинг робіт, а також інформація про роботи автори яких запрошено на підсумкову конференцію.)</h2>
-<p>Після офіційної розсилки запрошень до ВНЗ учасників конкурсу, а також після розислки запрошень на електронні поштові скриньки на цій сторінці можна буде завантажити відскановані копії запршень для оформлення відряджень.</p>
--->
-    <?php endif;?>
-<?php endif;?>
-<autor>Krupnik&copy;&nbsp;</autor>
+    <?php else: ?>
+        <h1>Конкурс СНР <?= NYEARS ?> н.р. завершено. Результати переміщено в архів.</h1>
+        <!--
+        Закоментировав предидущую строку раскоментируй следующую (2018.06.13)
+        <h1>Вибачте Реєст знаходиться в обробці. Завітайте на нашу сторінку пізніше.</h1>
+        <h2>(Зараз. Роботи отримують рецензію. Після цього процесу та підбиття підсумків буде оприлюднено рейтинг робіт, а також інформація про роботи автори яких запрошено на підсумкову конференцію.)</h2>
+        <p>Після офіційної розсилки запрошень до ВНЗ учасників конкурсу, а також після розислки запрошень на електронні поштові скриньки на цій сторінці можна буде завантажити відскановані копії запршень для оформлення відряджень.</p>
+        -->
+    <?php endif; ?>
+<?php endif; ?>
+<div class="autor">Krupnik&copy;&nbsp;</div>
 </body>
 </html>
